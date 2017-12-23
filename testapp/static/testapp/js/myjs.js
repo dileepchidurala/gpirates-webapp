e79054cd30f9143b7c363bb65563f6b95871a0ad
krishnasaurabh <krishnasaurabh123@gmail.com>
2017-12-23 20:42:59 +0530

    added report a problem

--- testapp/static/testapp/js/myjs.js
+++ testapp/static/testapp/js/myjs.js
@@ -32,3 +32,35 @@ $("#requestModal").on("click", function(){
   $("#success").hide();
 });
 
+$("#reportButton").on("click", function(){
+    var  name = "problem user"
+    var description = "PROBLEM: " + $("#problem").val();
+    $("#problem").val("");
+    $.ajax({
+      url: base + '/app/addrequest/',
+      dataType: "json",
+      type: "POST",
+      data: {
+        'name': name,
+        'requestDetail': description,
+      },
+      crossDomain: true,
+      success: function(result) {
+        $("#modalcontent").hide();
+        $("#success").show();
+        $("#success").html('<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>');
+        $("#success").append("<center>Your Problem has been reported. It will be handled.<br>Keep checking for new uploads :)</center><br><br>");
+        // $("#modalFooter").css("textAlign", "center");
+      },
+      error: function(resp, ajaxoption, thrownerror) {
+          console.log("error");
+      }
+    });
+  })
+  
+  $("#reportModal").on("click", function(){
+    $("#modalcontent").show();
+    $("#success").hide();
+  });
+  
+  
